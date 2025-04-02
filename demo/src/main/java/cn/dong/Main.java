bb9f16ba8f7795a9de529ed1e8b9919ffbae930e
Cola <alandong4748@gmail.com>
2025-04-02 20:24:16 +0800

    Add initial implementation of ACMInputHelper and BaseTest interfaces, and update Main class structure

--- demo/src/main/java/cn/dong/Main.java
+++ demo/src/main/java/cn/dong/Main.java
@@ -1,22 +1,19 @@
 package cn.dong;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * @author dongzhenxun
  * @date 2025/3/31 下午3:37
  * @description 描述
  */
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
---
+++ demo/src/main/java/cn/dong/exams/util/ACMInputHelper.java
@@ -0,0 +1,12 @@
+package cn.dong.exams.util;
+
+/**
+ * @author dongzhenxun
+ * @date 2025/3/31 下午8:47
+ * @description 描述
+ */
+public interface ACMInputHelper {
+
+
+
+}
